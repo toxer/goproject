fa6ab5b4e9872a715de6e10384cd5bf96013ad65
toxer <marco.tozzato@gmail.com>
2015-05-15 16:34:48 +0200

    Convertito in stringa la stat del file

--- hello/hello.go
+++ hello/hello.go
@@ -1,17 +1,19 @@
 package main
 import ("fmt"
+//"encoding/json"
 "os"
 
 
 )
 func main() {
-    path := "/tmp/data.txt"
-    fi, err := os.Stat(path)
-    if err != nil {
-        return
-    }
-   
-        fmt.Println(fi.Sys())
-      
-    
+	path := "/tmp/data.txt"
+	fi, err := os.Stat(path)
+	if err != nil {
+		return
+	}
+    //converto la stat del file in stringa
+    str:=fmt.Sprintln(fi.Sys())
+	fmt.Println(str)
+
+
 } 
